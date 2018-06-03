8c043eec5cdb23372fc21710df5cbd76f7551f62
suzymlewis <suzymlewis@gmail.com>
2018-06-03 08:24:11 -0700

    Update get_signal.m

--- Suzanne/Functions/get_signal.m
+++ Suzanne/Functions/get_signal.m
@@ -24,12 +24,10 @@ glomeruli = []; % Create an array to hold glomerular assigment for each row of s
 
 for i = 1:length(ROI) % LOOP OVER GLOMERULI
     temp_indx = ROI{i}; % Grab indices for ROI pixels of looping glomeruli
-      % This indices correspond to a 116 x 156 pixel tif image frame
+    % This indices correspond to a 116 x 156 pixel tif image frame
     glom_temp = (ones(1, length(ROI{i})) * i)'; % Make an array with glomeruli number as long as number of glomeruli in ROI
     glomeruli= vertcat(glomeruli, glom_temp); % Update array so will have glomeruli assignment for each row of signal
-    for ii = 1:size(temp_indx, 1) % LOOP THROUGH
-        for iii = 1: size(raw, 4) % LOOP THROUGH TRIALS
-        signal((size(signal,1)+1),:,iii) = raw_(temp_indx(ii),:,iii);
-    end
+    for ii = 1:size(temp_indx, 1) % LOOP THROUGH PIXELS
+            signal((size(signal,1)+1),:, :) = raw_(temp_indx(ii),:,:);
     end
 end
