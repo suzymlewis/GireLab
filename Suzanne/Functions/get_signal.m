afcbdcdcfa720f936c3cae06170c6c50b62488a5
suzymlewis <suzymlewis@gmail.com>
2018-06-03 07:32:45 -0700

    Add files via upload

---
+++ Suzanne/Functions/get_signal.m
@@ -0,0 +1,28 @@
+function [signal glomeruli] = get_signal(ROI, raw)
+
+%%%%%%%%%%%%%%% LOADS RELEVANT VARIABLES FROM SEGMENTED DATA INTO WORKSPACE
+    % INPUT
+        % ROI 
+    % OUTPUT
+        % signal: m x n matrix containing Ca signal for all pixels lie
+        % within ROI's designated by CalIm CNMF
+            % Rows :  Pixels that fall within ROI's
+            % Columns : samples for each timepoint sampled at 20 Hz
+        % glomeruli: n x 1 array designating the ROI assignment for each
+        % pixel row in the signal matrix
+%%%%%%%%%%%%%%% 
+
+% Reshape raw for indexing with ROI assignments
+raw_=reshape(raw, [(size(raw, 1)*size(raw, 2)), size(raw, 3)]);
+
+signal=[]; % Create empty matrix to store Raw downsampled (median filtered) Ca Imaging data
+    
+glomeruli = []; % Create an array to hold glomerular assigment for each row of signal matrix
+for i = 1:length(ROI)
+    temp_indx = ROI{i};
+    glom_temp = (ones(1, length(ROI{i})) * i)';
+    glomeruli= vertcat(glomeruli, glom_temp);
+    for ii = 1:size(temp_indx, 1)
+        signal((size(signal,1)+1),:) = raw_(temp_indx(ii),:);
+    end
+end
