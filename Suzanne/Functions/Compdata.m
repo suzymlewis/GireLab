1fa80184235debe09256e437c0f52d0524932a48
suzymlewis <suzymlewis@gmail.com>
2018-06-03 08:12:24 -0700

    Update Compdata.m

--- Suzanne/Functions/Compdata.m
+++ Suzanne/Functions/Compdata.m
@@ -19,7 +19,9 @@ Y_ca = comp.clc; % Downsampled raw data using median filter for neighboring pixe
 Y_ca = Y_ca (5:120, 5:160,:); % Drop areas of frame removed from motion correction to be consistent with segmented data
 frame_trial = comp.tr1a; % Vector containing trial assignment for each frame
 
-for i = 1:max(frame_trial) % LOOP THROUGH TRIALS
+trials = max(frame_trial); % Designate number of trials for output
+
+for i = 1:trials % LOOP THROUGH TRIALS
         k = find(frame_trial ==i); % Get indices current trials samples
         temp = Y_ca(:,:,k); % Create array with only current looping trials information
         raw(:,:,:,i) = temp(:,:,1:450);
